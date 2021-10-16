dc035b002fce7f5b77139eb8410cf05af2905d02
Mundhe, Chinmayee <cmundhe@iu.edu>
2021-10-16 16:13:43 -0400

    Initial checkin

---
+++ part1/raichu.py
@@ -0,0 +1,40 @@
+#
+# raichu.py : Play the game of Raichu
+#
+# PLEASE PUT YOUR NAMES AND USER IDS HERE!
+#
+# Based on skeleton code by D. Crandall, Oct 2021
+#
+import sys
+import time
+
+def board_to_string(board, N):
+    return "\n".join(board[i:i+N] for i in range(0, len(board), N))
+
+
+def find_best_move(board, N, player, timelimit):
+    # This sample code just returns the same board over and over again (which
+    # isn't a valid move anyway.) Replace this with your code!
+    #
+    while True:
+        time.sleep(1)
+        yield board
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        raise Exception("Usage: Raichu.py N player board timelimit")
+        
+    (_, N, player, board, timelimit) = sys.argv
+    N=int(N)
+    timelimit=int(timelimit)
+    if player not in "wb":
+        raise Exception("Invalid player.")
+
+    if len(board) != N*N or 0 in [c in "wb.WB@$" for c in board]:
+        raise Exception("Bad board string.")
+
+    print("Searching for best move for " + player + " from board state: \n" + board_to_string(board, N))
+    print("Here's what I decided:")
+    for new_board in find_best_move(board, N, player, timelimit):
+        print(new_board)
