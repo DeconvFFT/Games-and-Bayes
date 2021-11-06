d7145203bc121753e5f14b35aa09bb75acf3db19
Anitha Ganapathy <Anitha.g19@gmail.com>
2021-11-05 21:40:31 -0400

    increased depth

--- part1/raichu.py
+++ part1/raichu.py
@@ -514,7 +514,7 @@ def find_best_move(board, N, player, timelimit):
         listOfSuccessors = successors(board, N, player)
         if len(listOfSuccessors) > 0:
             successors_evaluation = []
-            depth = 5
+            depth = 3
             alpha = -99999
             beta = 99999
             for each_succ in listOfSuccessors:
